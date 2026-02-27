4d831767d8255aa18d677b232ccd0eacd79b6d19
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-27 15:19:18 +0200

    Added a New Project

---
+++ 06. Foreign Languages/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = Console.ReadLine();
+
+if (name == "England" || name == "USA")
+{
+    Console.WriteLine("English");
+}
+else if (name == "Spain" || name == "Argentina" || name == "Mexico")
+{
+    Console.WriteLine("Spanish");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
