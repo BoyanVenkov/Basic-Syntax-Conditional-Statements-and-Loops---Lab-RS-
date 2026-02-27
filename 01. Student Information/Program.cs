c66292c570eb5dd049158cd7c034d90711e7d806
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-27 14:44:53 +0200

    Add project files.

---
+++ 01. Student Information/Program.cs
@@ -0,0 +1,7 @@
+﻿// Name: John, Age: 15, Grade: 5.40
+
+string name = Console.ReadLine();
+string age = Console.ReadLine();
+string averageGrade = Console.ReadLine();
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade}");
