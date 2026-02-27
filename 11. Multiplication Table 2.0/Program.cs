6bb4b4fa6afbb02136f918a7ac34d99522fb31a7
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-27 15:48:31 +0200

    Added a New Solution

---
+++ 11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int result = num1 * num2;
+
+if (num2 > 10)
+{
+    Console.WriteLine($"{num1} X {num2} = {num1 * num2}");
+}
+
+for (int i = num2; i <= 10; i++)
+{
+    result = num1 * i;
+    Console.WriteLine($"{num1} X {i} = {result}");
+}
