0ad6e042fad01552e326e8ba5b8170c162f99416
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-27 15:40:21 +0200

    Added a New Solution

---
+++ 09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int input = int.Parse(Console.ReadLine());
+int sum = 0;
+int oddCount = 0;
+
+for (int i = 1; i<= input * 2; i ++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+        sum += i;
+        oddCount++;
+    }
+    if (oddCount == input)
+    {
+        Console.WriteLine($"Sum: {sum}");
+        break;
+    }
+    
+}
