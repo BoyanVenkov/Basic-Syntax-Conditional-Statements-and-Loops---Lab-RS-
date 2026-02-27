dad838358058150e6bb8e25bf0d40044159bda45
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-27 15:11:25 +0200

    Added a New Solution

---
+++ 04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,20 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= 30; i++)
+{
+    minutes++;
+    if (minutes == 60)
+    {
+        hours++;
+        minutes = 0;
+    }
+    if (hours == 24)
+    {
+        hours = 0;
+    }
+
+
+}
+Console.WriteLine($"{hours}:{minutes:d2}");
